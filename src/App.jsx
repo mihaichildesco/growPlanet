82f9eb134e8155460edf4d00bf08fb61cd97ee4c
Mihai Childesco <mihai.childesco@gmail.com>
2023-12-06 18:04:53 +0200

    changed background

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,31 @@ export default function App() {
     <>
       <Spline
         style={{ width: "100%", height: "100vh" }}
-        scene="https://prod.spline.design/LuV4t1YsuagRJ7bn/scene.splinecode"
+        scene="https://prod.spline.design/N7vfjT9DkkGBBbi7/scene.splinecode"
       />
+      <h1
+        style={{
+          position: "absolute",
+          top: "30%",
+          left: "30%",
+          transform: "translate(-50%, -50%)",
+          fontSize: "5rem",
+          color: "white",
+          textTransform: "uppercase",
+          boxShadow: "0 0 10px #000",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          padding: "1rem",
+          borderRadius: "10px",
+          zIndex: 1,
+        }}
+      >
+        Childesco Mihai
+      </h1>
     </>
   );
 }
+
+// scene="https://prod.spline.design/N7vfjT9DkkGBBbi7/scene.splinecode" />
+
+// planet
+// scene="https://prod.spline.design/LuV4t1YsuagRJ7bn/scene.splinecode"
