1414e4bc2a9ef3d637f517f8169e3c6a97118542
Mihai Childesco <mihai.childesco@gmail.com>
2023-12-06 17:39:34 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Spline from "@splinetool/react-spline";
+
+export default function App() {
+  return (
+    <>
+      <Spline
+        style={{ width: "100%", height: "100vh" }}
+        scene="https://prod.spline.design/LuV4t1YsuagRJ7bn/scene.splinecode"
+      />
+    </>
+  );
+}
